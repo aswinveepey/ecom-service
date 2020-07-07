fe74f1af6a3533199efd018e4cf31e5e3b5835b2
LIT-Admin <aswin@littech.in>
2020-07-07 11:17:31 +0530

    sentry dsn integration

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
+const Sentry = require("@sentry/node");
 var logger = require("morgan");
 const bodyParser = require("body-parser");
-var app = express();
 var cors = require("cors");
 
 //env config
@@ -10,19 +10,11 @@ require("dotenv").config();
 //db config
 require("./v1/db/db");
 
-//import auth middleware
-// const {auth} = require("./v1/middlewares/authMiddleware");
-
-//logger set up
-app.use(logger("dev"));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-
-//cors
-app.use(cors());
-
-//get versioned routes
-const routerV1 = require("./v1/routes/index");
+//Sentry init
+Sentry.init({
+  dsn:
+    process.env.SENTRY_DSN,
+});
 
 /**
  * Swagger related setup
@@ -31,12 +23,28 @@ const swaggerUi = require("swagger-ui-express");
 const swaggerJSDoc = require("swagger-jsdoc");
 const options = require("./v1/swaggerDef");
 const swaggerSpec = swaggerJSDoc(options);
+
+//get versioned routes
+const routerV1 = require("./v1/routes/index");
+
+// create Express APP
+var app = express();
+
+//logger set up
+// The request handler must be the first middleware on the app
+app.use(Sentry.Handlers.requestHandler());
+//cors
+app.use(cors());
+//logger middleware and config
+app.use(logger("dev"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+//swagger middleware
 app.use(
   "/api-docs",
   swaggerUi.serve,
   swaggerUi.setup(swaggerSpec, false, { docExpansion: "none" })
 );
-
 // parse application/json
 app.use(bodyParser.json());
 /**
@@ -44,6 +52,6 @@ app.use(bodyParser.json());
  */
 app.use("/api/v1/:tenantId", routerV1);
 
-app.listen(process.env.PORT || 3002, () =>
-  console.log(`Service ready & listening at port: ` + process.env.PORT)
+app.listen(process.env.SERVICE_PORT || 3002, () =>
+  console.log(`Service ready & listening at port: ` + process.env.SERVICE_PORT)
 );
