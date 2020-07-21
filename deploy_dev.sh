398f09083364b51d9daf6382913cd0c0cfaea4c8
LIT-Admin <aswin@littech.in>
2020-07-21 11:56:59 +0530

    update deployment files

--- deploy_dev.sh
+++ deploy_dev.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-eval $(aws ecr get-login --no-include-email --region ap-south-1 --profile litcomdev)
+aws ecr get-login-password --region ap-south-1 --profile litcomdev | docker login --username AWS --password-stdin 791195057071.dkr.ecr.ap-south-1.amazonaws.com
 docker build -t litcom-service-rest:latest -f docker/Dockerfile .
 docker tag litcom-service-rest:latest 791195057071.dkr.ecr.ap-south-1.amazonaws.com/litcom-service-rest:latest
 docker push 791195057071.dkr.ecr.ap-south-1.amazonaws.com/litcom-service-rest:latest
