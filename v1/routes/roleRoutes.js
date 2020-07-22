1e371eb5a1cd09aa3a86a959afc0e795ad2e177e
LIT-Admin <aswin@littech.in>
2020-07-22 19:25:38 +0530

    admin role permission to role router

--- v1/routes/roleRoutes.js
+++ v1/routes/roleRoutes.js
@@ -2,8 +2,9 @@ const express = require('express')
 const roleRouter = express.Router()
 const roleController = require('../controllers/roleController')
 const { user } = require("../middlewares/user");
+const { admin } = require("../middlewares/role");
 
-roleRouter.get('/', user, roleController.getRoles)
-roleRouter.post('/', user, roleController.createRoles)
+roleRouter.get('/', user, admin, roleController.getRoles)
+roleRouter.post('/', user, admin , roleController.createRoles)
 
 module.exports = { roleRouter };
