55269d2daec1c8f81840901c9990d0067c9f47e8
LIT-Admin <aswin@littech.in>
2020-07-22 21:36:08 +0530

    admin  validation user and roles

--- v1/routes/roleRoutes.js
+++ v1/routes/roleRoutes.js
@@ -4,7 +4,7 @@ const roleController = require('../controllers/roleController')
 const { user } = require("../middlewares/user");
 const { admin } = require("../middlewares/role");
 
-roleRouter.get('/', user, admin, roleController.getRoles)
+roleRouter.get('/', user, roleController.getRoles)
 roleRouter.post('/', user, admin , roleController.createRoles)
 
 module.exports = { roleRouter };
--- v1/routes/userRoutes.js
+++ v1/routes/userRoutes.js
@@ -2,14 +2,14 @@ const express = require("express");
 const userRouter = express.Router();
 const userController = require("../controllers/userController");
 const { user } = require("../middlewares/user");
+const { admin } = require("../middlewares/role");
 
 userRouter.get("/", user, userController.getAllUsers);
 userRouter.get("/dash", user, userController.getUserDash);
 userRouter.get("/nav", user, userController.getUserNav);
 userRouter.get("/id/:userId", user, userController.getOneUser);
-userRouter.post("/", user, userController.createUser);
-userRouter.post("/id/:userId", user, userController.updateUser);
+userRouter.post("/", user,admin, userController.createUser);
+userRouter.post("/id/:userId", user,admin, userController.updateUser);
 userRouter.post("/search", user, userController.searchUser);
-// userRouter.get("/search", userController.searchUser);
 
 module.exports = { userRouter };
